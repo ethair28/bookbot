5ec28eea9f962152b4cd584723fc07ad4b409689
ethair28 <etthai1010@gmail.com>
2025-10-22 00:32:46 +0300

    completed the program it now prints in an asthetic manner data about the input (books)

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from stats import word_counter, character_counter 
+from stats import word_counter, character_counter, sort_on, dict_to_sort_list, dis_clean_data 
 
 def get_book_txt(path_to_text: str) ->str:
     with open(path_to_text) as f:
         return f.read()
     
 def main(path_to_text: str):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_text}...")
+    print("----------- Word Count ----------")
     book_txt = get_book_txt(path_to_text)
     num_words = word_counter(book_txt)
-    num_chars_dict = character_counter(book_txt)
     print(f"Found {num_words} total words")
-    print(num_chars_dict)
+    print("--------- Character Count -------")
+    num_chars_dict = character_counter(book_txt)
+    sorted_list = dict_to_sort_list(num_chars_dict)
+    dis_clean_data(sorted_list)
+    print("============= END ===============")
     return None
 
 
--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 # this function takes in a string, splits the string into a list of words and returns the length on the list (number of words)
-def word_counter(book_txt:str):
+def word_counter(book_txt:str) ->int:
     num_of_words = len(book_txt.split())
     return num_of_words
 # turns any upercase into lowercase, counts the number of charecters and returns a dict with the number of tims each catacter showed up 
@@ -10,6 +10,21 @@ def character_counter(book_txt: str) ->dict:
         if char in char_count_dict:
             char_count_dict[char] += 1
         else:
-            char_count_dict[char] = 1
-    return char_count_dict            
+            char_count_dict[char] = 1         
+    return char_count_dict  
+          
+def sort_on(items):
+    return items["count"]
+
+def dict_to_sort_list(char_count_dict: dict) ->list:
+    dict_to_list = [{"character": char, "count": count} for char, count in char_count_dict.items()]
+    dict_to_list.sort(reverse=True, key=sort_on)
+    return dict_to_list
+
+def dis_clean_data(dict_to_list: list):
+    for char_data in dict_to_list:
+        print(f'{char_data["character"]}: {char_data["count"]}')
+    return None    
+
+
 
